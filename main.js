6a61173f7dbc579cb4e7e3391b2a2216751495e6
Tancredi Carluccio <tanc_@hotmail.it>
2022-04-11 02:45:00 +0200

    Stampa email

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ var app = new Vue(
         el: "#latenight",
         data: {
 
+            emailRandom: "",
+
         },
 
         created(){
 
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-                 .then(function(response) {
+                 .then((response) => {
+                     console.log(this);
                      console.log(response.data.response);
+                     this.emailRandom = response.data.response;
                  });
         },
         
