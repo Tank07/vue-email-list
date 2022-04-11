ea9625bae7e3d52e1237611e862caf688e4b84db
Tancredi Carluccio <tanc_@hotmail.it>
2022-04-12 00:44:52 +0200

    Esercizio completato

--- main.js
+++ main.js
@@ -3,18 +3,24 @@ var app = new Vue(
         el: "#latenight",
         data: {
 
-            emailRandom: "",
+            emailRandom: [],
 
         },
 
         created(){
 
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            for (let i = 1; i <= 10; i++) {
+
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
                  .then((response) => {
                      console.log(this);
                      console.log(response.data.response);
-                     this.emailRandom = response.data.response;
-                 });
+                     this.emailRandom.push(response.data.response);
+                 });               
+                
+            }
+
+            
         },
         
         methods: {
